48ee2ae098bc5247df3be825dc938e3e8df33877
rage10k <richemman81@gmail.com>
2017-06-19 08:30:14 +0100

    Open University
    Maps practice

---
+++ dataTrans.java
@@ -0,0 +1,58 @@
+/*#########################################################################
+##                                                                       ##
+##  #######     ####     ######   ########     ##     ####     ##   ###  ##
+##  ##    ##   ##  ##   ##    ##  ##         ####    ##  ##    ##  ##    ##
+##  ##    ##  ##    ##  ##        ##           ##   ##    ##   ## ##     ##
+##  ##   ###  ##    ##  ##        ##           ##   ##    ##   ####      ##
+##  #######   ########  ##  ####  #######      ##   ## ## ##   ##        ##
+##  ##   ##   ##    ##  ##    ##  ##           ##   ##    ##   #####     ##
+##  ##    ##  ##    ##  ##    ##  ##           ##   ##    ##   ## ##     ##
+##  ##    ##  ##    ##  ##    ##  ##           ##    ##  ##    ##  ##    ##
+##  ##    ##  ##    ##   ######   ########    ####    ####     ##   ###  ##
+##                                                                       ##
+##  Date :    27.05.17                                                   ##
+##  Event :   <>                                                         ##
+##  Author :  R.Emmanuel                                                 ##
+##  Email :   rage10@hotmail.co.uk                      	         ##
+##  Version : v0.1                                                       ##
+##  Source :  Java 1.8                                                   ## 
+#########################################################################*/
+/**
+ * This is been created to practice the work necessary for the university
+ * course that I have enrolled on. I am going to cover reader and write 
+ * streams.
+ */
+
+import java.util.*;
+import java.lang.*;
+import ou.*;
+
+
+class FileReader
+{
+   public FileReader()
+   {
+      OUFileChoooser.SetPath(“/User/Xavier/Desktop/“);
+      String pathname = OUFileChooser.getFilename(“emmanuel.txt”);
+      File aFile =  new File(pathname);
+      
+      try
+      {
+         FileReader fileRead = new FileReader(aFile);
+         String ch = FileReader.read();
+	
+   	 while(!ch = -1)
+	 {
+	    System.out.println((Char) ch);
+            ch = fileReader.read();
+         }  // end while loop.
+         fileReader.close();
+      }
+      catch(Exception ex)
+      {
+         System.out.print(“Error msg: ” + ex);
+      }   // end of the try-catch statement.
+   }   // end of Constructor.
+}   // end of FileReader.
+
+
---
+++ maps.java
@@ -0,0 +1,208 @@
+/*###########################################################################################
+##                                                                                         ##
+##  ########  ##       ##  ##       ##    #####    ##     ##  ##     ## ######## ##        ##
+##  ##        ###     ###  ###     ###   ##   ##   ##     ##  ##     ## ##       ##        ## 
+##  ##        ####   ####  ####   ####  ##     ##  ###    ##  ##     ## ##       ##        ##
+##  ##        ## ## ## ##  ## ## ## ##  ##     ##  ## #   ##  ##     ## ##       ##        ##
+##  #######   ##  ###  ##  ##  ###  ##  #########  ##  #  ##  ##     ## #######  ##        ##
+##  ##        ##       ##  ##       ##  ##     ##  ##   # ##  ##     ## ##       ##        ##
+##  ##        ##       ##  ##       ##  ##     ##  ##    ###  ##     ## ##       ##        ##
+##  ##        ##       ##  ##       ##  ##     ##  ##     ##   ##   ##  ##       ##        ##
+##  ########  ##       ##  ##       ##  ##     ##  ##     ##    #####   ######## ########  ##
+##                                                                                         ##
+##  Date :   27.05.17                                                                      ##
+##  Company: Open University                                                               ##
+##  Author:  R.Emmanuel                                                                    ##
+##  Email:   richard.emmanuel@live.co.uk			                           ##
+##  Version: v0.1                                                                          ##
+##  Source:  Java 1.8                                                                      ## 
+###########################################################################################*/
+/**
+ * This source code has been put together to demonstrate how to use some collection Map and
+ * array.
+ */
+
+import java.util.*;
+import java.lang.*;
+import ou.*;
+
+
+class Player
+{
+   private int goals;
+   private String name;
+   
+   public Player()
+   {
+      this.goals() = 0;
+      this.name() = null;
+   }
+ 
+   public int getGoals()
+   {
+      return this.goals();
+   }
+
+   public String getName()
+   {
+      return this.name();
+   }
+
+   public void setName(String aName)
+   {
+      this.name = aName;      
+   }
+	
+   public toString()
+   {
+      if(this.getGoals() != 0)
+	{
+           return “Player ”+ this.getName() + “has scored ” + this.goals;
+        }
+        else
+        {
+           return “Player ”+ this.getName() + “has scored no goals”;       
+	}
+   }
+}
+
+
+
+// Question 2
+interface Feedable
+{
+
+  public boolean feed();
+}
+
+
+
+
+HungryFrog extends Frog implements Feedable
+{
+  private int energyLevel;
+  private final int FULL = 2000;	
+  
+  public HungryFrog()
+  {
+    super();
+    this.energyLevel = this.FULL;
+    
+  }
+
+  public int getEnergyLevel()
+  {
+    return this.energyLevel;
+  }
+
+  public void setEnergyLevel(int x)
+  {
+    this.energyLevel = x;
+  }
+ 
+
+ 
+  @Override
+  public void setPosition (int aPosition)
+  {
+    int x = getEnergyLevel();
+    int y = aPosition * 100;
+
+    if(x >= y)
+    {
+      super.position(aPosition);
+      this.update("position");
+      this.setEnergyLevel(this.getEnergyLevel() - (aPosition * 100));
+    }
+    else
+    {
+      OUDialog.alert(“insufficient energy levels to move!”);
+    }
+  }
+
+
+
+  public boolean feed()
+  {
+    int x = this.getPosition();   
+    System.out.println(x);
+    if((x >= 1) && (x <= 10))
+    {
+      while (this.getEnergyLevel() < FULL )
+      {
+         this.croak();
+         this.setEnergyLevel(this.getEnergyLevel() + (100));
+      }
+    }
+    else
+    {
+      System.out.println("out of range!");
+      if( x < 1)
+      {
+         super.setPosition(1);
+         this.feed();
+      }
+      else if (x > 10)
+      {
+         super.setPosition(10);
+         this.feed();
+       }
+    }
+    return true;
+  }
+
+  
+}
+
+Question 3
+
+int[] temp = new int[11];
+
+
+Set<String> myList = new HashSet<>();
+
+Map<String, Integer> myList = new HashMap<>();
+
+
+private Map<String, List<String>> bulbMap;
+
+
+public BulbCatalogue(){
+   this.buldMap = new TreeMap<>();
+}
+
+
+public void addBulbType(String bulbType){
+   this.bulbMap.put(bulbType, new ArrayList<String>());
+}
+
+
+public void addVariety(String bulbType, String variety){
+   this.bulbMap.get(bulbType).add(Variety);
+}
+
+
+public void removeVariety(String bulbType, String variety){
+   this.bulbMap.get(BulbType).remove(variety);
+}
+
+
+public void printVarieties(String bulbType){
+   System.out.println(“Our varieties of ” + BulbType + are :”);
+   
+   for(String variety : this.bulbMap.get(bulbType)){
+      System.out.println(variety);
+   }
+}
+
+
+public void addBulbType(String bulbType){
+   if(!(BulbMap.keySet().contains(bulbType))){
+      bulbMap.put(bulbType, new ArrayList<String>())
+   }
+}
+
+
+
+
+
