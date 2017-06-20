26f071ffb90cf5537fc45186e5fbe24e19366a9e
Emmanuel <richemman81@gmail.com>
2017-06-20 11:25:28 +0100

    Update and rename splitArray to splitArray.java

---
+++ splitArray.java
@@ -0,0 +1,8 @@
+ArrayList<String> myStrings  = new ArrayList<String>(Arrays.asList("apple","rose","water","car","bike","dog"));
+
+string[] stringList = data.getData().split("( )"); 
+
+for (String currString : stringList){ 
+	myStrings.remove(currString ); 
+} 
+
